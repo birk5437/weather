868e491bfec1fade65a939b078991d798c9eaaab
Burke Ramsey <birk5437@gmail.com>
2013-10-20 21:55:27 -0400

    fixed api call

--- app/models/location.rb
+++ app/models/location.rb
@@ -15,8 +15,14 @@ class Location < ActiveRecord::Base
   end
 
   def current_conditions
-    url = "http://free.worldweatheronline.com/feed/weather.ashx?q=" + zip + "&format=json&key=55d99285d6001415122608"
+    # http://developer.worldweatheronline.com/documentation
+    # url = "http://free.worldweatheronline.com/feed/weather.ashx?q=" + zip + "&format=json&key=55d99285d6001415122608"
+    url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=" + zip + "&format=json&key=z8qme23navrzw7dq9hp7mudf"
     response = JSON.parse(RestClient.get(url))["data"]["current_condition"].first
+  end
+
+  def current_conditions_icon_url
+    current_conditions["weatherIconUrl"].first["value"]
   end
 
   def self.popular_locations
