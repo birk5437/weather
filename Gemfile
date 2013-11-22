2f4ca0d65c4ec3dae8b1b7fcd0859c1e724adde9
Burke Ramsey <birk5437@gmail.com>
2013-11-21 23:18:20 -0500

    Radar images and more styling

--- Gemfile
+++ Gemfile
@@ -8,3 +8,4 @@ gem "inherited_resources"
 gem "rest-client", "~> 1.6.7"
 gem "json"
 # gem "geocoder", "~> 1.1.8"
+# gem "awesome_print", :group => [:development]
--- app/models/location.rb
+++ app/models/location.rb
@@ -65,10 +65,9 @@ class Location < ActiveRecord::Base
     # url = "http://free.worldweatheronline.com/feed/weather.ashx?q=" + zip + "&format=json&key=55d99285d6001415122608"
     url = "http://api.worldweatheronline.com/free/v1/weather.ashx?q=" + zip + "&format=json&key=z8qme23navrzw7dq9hp7mudf"
     response = JSON.parse(RestClient.get(url))["data"]["current_condition"].first
-  end
-
-  def current_conditions_icon_url
-    current_conditions["weatherIconUrl"].first["value"]
+    response["weatherDesc"] = (response["weatherDesc"].first["value"] rescue "")
+    response["weatherIconUrl"] = (response["weatherIconUrl"].first["value"] rescue "")
+    response
   end
 
   def nearest_radar_location
